f29e68834611866e11450e5fd857f7e73da504cd
hadanur <115171695+hadanur@users.noreply.github.com>
2022-11-24 17:54:39 +0300

    Initial Commit

---
+++ Mortage Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mortage Calculator
+//
+//  Created by Hakan Adanur on 24.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
